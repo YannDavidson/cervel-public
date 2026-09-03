65c4a52e73b83a12a9353e4a9c1612b2528799a2
Picsou Balthazar <30988886+YannDavidson@users.noreply.github.com>
2026-09-02 21:54:50 -0500

    Expose public Python SDK models

---
+++ sdk/python/cervel_public/__init__.py
@@ -0,0 +1,25 @@
+"""Public Python helpers for CERVEL experimental interoperability drafts."""
+
+from .models import (
+    PUBLIC_DRAFT_VERSION,
+    CapabilityContract,
+    CapabilityDiscovery,
+    CaptureEnvelope,
+    ErrorEnvelope,
+    KnowledgeReference,
+    LookupRequest,
+    LookupResult,
+    LookupResultItem,
+)
+
+__all__ = [
+    "PUBLIC_DRAFT_VERSION",
+    "CapabilityContract",
+    "CapabilityDiscovery",
+    "CaptureEnvelope",
+    "ErrorEnvelope",
+    "KnowledgeReference",
+    "LookupRequest",
+    "LookupResult",
+    "LookupResultItem",
+]
